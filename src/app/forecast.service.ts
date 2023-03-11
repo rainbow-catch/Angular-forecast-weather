aead74cf77e5659ab3e465a94c346fdef68d3d75
milosbayar <milosbayar114@gmail.com>
2023-03-11 19:29:31 +0100

    added removePolishLetters Method

--- src/app/forecast.service.ts
+++ src/app/forecast.service.ts
@@ -8,6 +8,20 @@ export class ForecastService {
   constructor() {}
 
   getForecast(location: string) {
+    const removePolishLetters = (input:string) => {
+      let result = input;
+      result = result.split("ó").join("o");
+      result = result.split("ź").join("z");
+      result = result.split("ł").join("l");
+      result = result.split("ą").join("a");
+      result = result.split("ę").join("e");
+      result = result.split("ć").join("c");
+      result = result.split("ń").join("n");
+      result = result.split("ś").join("s");
+      result = result.split("ż").join("z");
+      return result;
+    }
+    location = removePolishLetters(location);    
     const apiKey = `http://api.weatherapi.com/v1/current.json?key=61dab060f224404b838130524230903&q=${location}&aqi=no`;
     const res = axios
       .get(apiKey, { responseEncoding: 'utf8' })
