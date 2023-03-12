d134f24785dcf0525834cc2fdfed3424938a55d8
milosbayar <milosbayar114@gmail.com>
2023-03-12 18:59:33 +0100

    added more weather icons

--- src/app/forecast.service.ts
+++ src/app/forecast.service.ts
@@ -9,20 +9,20 @@ export class ForecastService {
   constructor() {}
 
   getForecast(location: string) {
-    const removePolishLetters = (input:string) => {
+    const removePolishLetters = (input: string) => {
       let result = input;
-      result = result.split("ó").join("o");
-      result = result.split("ź").join("z");
-      result = result.split("ł").join("l");
-      result = result.split("ą").join("a");
-      result = result.split("ę").join("e");
-      result = result.split("ć").join("c");
-      result = result.split("ń").join("n");
-      result = result.split("ś").join("s");
-      result = result.split("ż").join("z");
+      result = result.split('ó').join('o');
+      result = result.split('ź').join('z');
+      result = result.split('ł').join('l');
+      result = result.split('ą').join('a');
+      result = result.split('ę').join('e');
+      result = result.split('ć').join('c');
+      result = result.split('ń').join('n');
+      result = result.split('ś').join('s');
+      result = result.split('ż').join('z');
       return result;
-    }
-    location = removePolishLetters(location);    
+    };
+    location = removePolishLetters(location);
     const apiKey = `http://api.weatherapi.com/v1/current.json?key=61dab060f224404b838130524230903&q=${location}&aqi=no`;
     const res = axios
       .get(apiKey, { responseEncoding: 'utf8' })
@@ -35,14 +35,26 @@ export class ForecastService {
     return res;
   }
 
-  returnWeatherIcon(condition:conditionType, isDay: number) {
-    if (condition.code===1000) {
-      if (isDay===1) {
+  returnWeatherIcon(condition: conditionType, isDay: number) {
+    if (condition.code === 1000) {
+      if (isDay === 1) {
         return 'sun';
+      } else {
+        return 'moon-with-stars';
       }
-      else {
-        return 'moon-with-stars'
+    }
+    if (condition.code === 1003) {
+      if (isDay === 1) {
+        return 'cloudy-with-sun';
+      } else {
+        return 'moon-with-stars';
       }
+    }
+    if (condition.code === 1009 || condition.code === 1006) {
+      return 'cloudy';
+    }
+    if (condition.code === 1063 ) {
+      return 'cloud-little-rain'
     }
     return 'default';
   }
